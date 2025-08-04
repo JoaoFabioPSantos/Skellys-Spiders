6aee140d72c447795909985402686fb62de6b16b
JoaoFabioPSantos <joaofabiopreira@gmail.com>
2025-08-03 23:02:49 -0300

    feat enemy

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -5,6 +5,28 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     public int damage = 10;
+    public Animator animator;
+
+    public string triggerAttack = "Attack";
+    public string triggerKill = "Death";
+    public HealthBase health;
+    //essa variável foi redundante, health base já tem isso:
+    public float timeToDestroy = 1f;
+
+    private void Awake()
+    {
+        if (health != null)
+        {
+            health.OnKill += OnEnemyKill;
+        }
+        //precisamos destruir o callback para ele não ficar na memória.
+    }
+
+    private void OnEnemyKill()
+    {
+        health.OnKill -= OnEnemyKill;
+        Destroy(gameObject, timeToDestroy);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -16,5 +38,10 @@ public class EnemyBase : MonoBehaviour
             health.Damage(damage);
         }
 
+    }
+
+    public void Damage(int amount)
+    {
+        health.Damage(amount);
     }
 }
--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthBase : MonoBehaviour
 {
+    public Action OnKill;
+
     public int startLife = 10;
 
     public bool destroyOnKill = false;
@@ -42,6 +45,8 @@ public class HealthBase : MonoBehaviour
         {
             Destroy(gameObject, dellayToKill);
         }
+
+        OnKill?.Invoke();
     }
 
 }
